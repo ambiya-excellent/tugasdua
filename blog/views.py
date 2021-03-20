8052aa06fa0ae65be90c1a95ebde32cc22f3c397
ambiya-excellent <ambiyahafitz26@gmail.com>
2021-03-20 12:25:45 +0700

    commitpertama

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Artikel
+
+def index(request):
+	context = {
+	'articles': Artikel.objects.all().order_by('-created_date')
+
+	}
+	return render(request, 'blog/index.html', context)
+
+
+# Create your views here.
+def details(request, article_id):
+	context = {
+	'article': Artikel.objects.get(id=article_id)
+	}
+	return render(request, 'blog/details.html', context)
